82f45ee0255525017cbbc4b471b41a1fc3baaa35
nyzs <oknakoca@gmail.com>
2025-02-09 10:09:27 +0100

    fix(ui): remove redundant div to fix it from being hidden, fix header alignment

--- components/mail/mail.tsx
+++ components/mail/mail.tsx
@@ -89,74 +89,72 @@ export function Mail({ mails }: MailProps) {
         >
           <ResizablePanel defaultSize={isMobile ? 100 : 35} minSize={isMobile ? 100 : 35}>
             <div className="flex-1 overflow-y-auto">
-              <div>
-                <div className="sticky top-0 z-10 rounded-t-md bg-background pt-[6px]">
-                  <div className="flex items-center justify-between px-2">
-                    <div className="flex items-center gap-1">
-                      <SidebarToggle className="h-fit px-2" />
-                      <React.Suspense>
-                        <ComposeButton />
-                      </React.Suspense>
-                    </div>
-                    <div className="relative flex-1 px-4 md:max-w-[400px] md:px-8">
-                      <Search className="absolute left-6 top-1/2 h-3 w-3 -translate-y-1/2 text-muted-foreground md:left-10" />
-                      <Input
-                        placeholder="Search"
-                        className="h-7 w-full pl-7 focus-visible:ring-0 focus-visible:ring-offset-0"
-                      />
-                    </div>
-                    <div className="flex items-center space-x-1.5">
-                      <Button
-                        variant="ghost"
-                        className="md:h-fit md:px-2"
-                        onClick={() => setIsCompact(!isCompact)}
-                      >
-                        <AlignVerticalSpaceAround />
-                      </Button>
-                      <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                          <Button variant="ghost" className="md:h-fit md:px-2">
-                            <ListFilter className="h-4 w-4" />
-                          </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                          <DropdownMenuItem onClick={() => setFilterValue("all")}>
-                            All mail
-                          </DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => setFilterValue("unread")}>
-                            Unread
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                      <Filters />
-                    </div>
+              <div className="sticky top-0 z-10 rounded-t-md bg-background pt-[6px]">
+                <div className="flex items-center justify-between px-2">
+                  <div className="flex items-center gap-1">
+                    <SidebarToggle className="h-fit px-2" />
+                    <React.Suspense>
+                      <ComposeButton />
+                    </React.Suspense>
                   </div>
-                  <Separator className="mt-2" />
+                  <div className="relative flex-1 px-4 md:max-w-[400px] md:px-8">
+                    <Search className="absolute left-6 top-1/2 h-3 w-3 -translate-y-1/2 text-muted-foreground md:left-10" />
+                    <Input
+                      placeholder="Search"
+                      className="h-7 w-full pl-7 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </div>
+                  <div className="flex items-center space-x-1.5">
+                    <Button
+                      variant="ghost"
+                      className="md:h-fit md:px-2"
+                      onClick={() => setIsCompact(!isCompact)}
+                    >
+                      <AlignVerticalSpaceAround />
+                    </Button>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="md:h-fit md:px-2">
+                          <ListFilter className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setFilterValue("all")}>
+                          All mail
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setFilterValue("unread")}>
+                          Unread
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                    <Filters />
+                  </div>
                 </div>
+                <Separator className="mt-2" />
+              </div>
 
-                <div className="h-[calc(93vh)]">
-                  {filterValue === "all" ? (
-                    filteredMails.length === 0 ? (
-                      <div className="p-8 text-center text-muted-foreground">
-                        No messages found | Clear filters to see more results
-                      </div>
-                    ) : (
-                      <MailList
-                        items={filteredMails}
-                        isCompact={isCompact}
-                        onMailClick={() => setIsDialogOpen(true)}
-                      />
-                    )
-                  ) : filteredMails.filter((item) => !item.read).length === 0 ? (
-                    <div className="p-8 text-center text-muted-foreground">No unread messages</div>
+              <div className="h-[calc(93vh)]">
+                {filterValue === "all" ? (
+                  filteredMails.length === 0 ? (
+                    <div className="p-8 text-center text-muted-foreground">
+                      No messages found | Clear filters to see more results
+                    </div>
                   ) : (
                     <MailList
-                      items={filteredMails.filter((item) => !item.read)}
+                      items={filteredMails}
                       isCompact={isCompact}
                       onMailClick={() => setIsDialogOpen(true)}
                     />
-                  )}
-                </div>
+                  )
+                ) : filteredMails.filter((item) => !item.read).length === 0 ? (
+                  <div className="p-8 text-center text-muted-foreground">No unread messages</div>
+                ) : (
+                  <MailList
+                    items={filteredMails.filter((item) => !item.read)}
+                    isCompact={isCompact}
+                    onMailClick={() => setIsDialogOpen(true)}
+                  />
+                )}
               </div>
             </div>
           </ResizablePanel>
